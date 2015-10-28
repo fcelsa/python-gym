d7ba1c5d80327d82c0dc290afe6111b3e9a20276
Fabio Celsalonga <fabio.celsa@gmail.com>
2015-10-28 19:16:45 +0100

    small fix

--- year_cal.py
+++ year_cal.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 # year_cal
-# 
+#
 # GNU Public License version 3 - See http://www.gnu.org/licenses/licenses.html
-# 
+#
 # (c) Fabio Celsalonga
 #
 # un compatto calendario, ideale per essere utilizzato con tool tipo GeekTool
 # visualizza il mese precedente al corrente e quelli successivi, per il totale
 # impostato da numm
 #
-# Ver. 1.1 2015-10-28
+# Ver. 1.2 2015-10-28
 
 import calendar
 import datetime
@@ -18,11 +18,11 @@ import locale
 
 locale.setlocale(locale.LC_ALL, '')
 
-numm=5
+numm=5 # numero totale di mesi da visualizzare
 now=time.localtime()
-curj=now[0] if now[1]>1 else now[0]-1  
-curm=now[1]-1 if now[1]>1 else 12 
-ddw=1
+curj=now[0] if now[1]>1 else now[0]-1
+curm=now[1]-1 if now[1]>1 else 12
+ddw=1 # set al primo del mese la variabile giorno per ricavare i numeri di settimana
 
 for i in range(numm,0,-1):
     kw=datetime.date(curj, curm, ddw).isocalendar()[1]
@@ -36,10 +36,10 @@ for i in range(numm,0,-1):
             line = line + (' {:2d}'.format(d) if (d>0) else '   ')
             if d==now[2] and curm==now[1]:
                 line += '\033[0m'
-            if d > 1 and d < 31 and datetime.date(curj, curm, d).isocalendar()[2] == 4: 
+            if d > 1 and d < 31 and datetime.date(curj, curm, d).isocalendar()[2] == 4:
                 ddw = d
         print line
-        
+
         # calcolo quale sara' la prossima settimana perche' se mi limito ad incrementare kw di 1 (kw +=1) al cambio di anno ho problemi...
         nextweek = datetime.date(curj,curm, ddw) + datetime.timedelta(weeks=1)
         kw=nextweek.isocalendar()[1]
@@ -53,4 +53,3 @@ for i in range(numm,0,-1):
         ddw = 1
     if i>1:
         print
-
